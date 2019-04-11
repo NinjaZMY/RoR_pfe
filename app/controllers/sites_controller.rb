6519aaebc27cb1ed1198a535c35f5334f4f3ab91
Z M Y <modtafrik@gmail.com>
2019-04-11 21:13:06 +0100

    Initial commit

---
+++ app/controllers/sites_controller.rb
@@ -0,0 +1,48 @@
+class SitesController < ApplicationController
+  before_action :current_site, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @sites = Site.all
+  end
+
+  def show
+
+  end
+
+  def new
+    @site = Site.new
+  end
+
+  def create
+    site = Site.create(site_params)
+
+    redirect_to site_path(site)
+  end
+
+  def edit
+
+  end
+
+  def update
+    @site.update(site_params)
+
+    redirect_to site_path(@site)
+  end
+
+  def destroy
+    @site.destroy
+
+    redirect_to sites_path
+  end
+
+  private
+
+  def site_params
+    params.require(:site).permit(:nom, :url)
+  end
+
+  def current_site
+    @site = Site.find(params[:id])
+  end
+
+end
---
+++ db/seeds.rb
@@ -0,0 +1,3 @@
+Site.create(nom: 'facebook', url: 'http://facebook.com')
+Site.create(nom: 'yahoo', url: 'http://yahoo.com')
+Site.create(nom: 'instagram', url: 'http://instagram') 
