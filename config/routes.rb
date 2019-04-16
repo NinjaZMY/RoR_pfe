41c7a8ef1137f215d2171f628c4f2adbc681e6ff
Z M Y <modtafrik@gmail.com>
2019-04-16 15:01:25 +0100

    Successfully added devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
- resources :sites
- root "sites#index"
+  resources :sites
+  
+  devise_for :users
+  as :user do 
+  get "signin" => "devise/sessions#new"
+  delete "logout" => "devise/sessions#destroy"
+  get "signup" => "devise/registrations#new"
+  root "devise/sessions#new"
+  
+  end
+
+
+
 end
