40ee4f250f0a8d68dfa38d68f92d4c996100e33f
Z M Y <modtafrik@gmail.com>
2019-04-16 21:15:22 +0100

    Fixed bug related to Edit , via creating a function for both create & Update , respecting The DRY concept

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,7 +1,7 @@
 require 'open-uri'
 class SitesController < ApplicationController
   before_action :current_site, only: [:show, :edit, :update, :destroy]
-
+  before_action :Before_Create_Update , only: [:create,:update]
   def index
     @sites = Site.all
   end
@@ -17,45 +17,26 @@ class SitesController < ApplicationController
 
   def create
     
-      begin
-        $e=""#connected
-        connexion = Nokogiri::HTML(open('https://www.google.com/'))
-        rescue Exception => $e
-      end     
-        
-        if ($e!="")
-        redirect_to new_site_path
-        else
-          begin
-          $e=""#connected
-          content = Nokogiri::HTML(open(site_params[:url]))
-          rescue Exception => $e
-          end     
-          
-          if ($e!="") #not connected
-          redirect_to new_site_path
-          else
-          #:nom , :url , :user_id
-          user_id = current_user[:id] 
-          url = site_params[:url] 
-          nom = site_params[:nom]
-          site = Site.create(:nom => nom , :url => url ,
-           :user_id => user_id )
-          # do the next thing
-          content=content.to_s  
-
+     
+  
+    #:nom , :url , :user_id
+    
+    site = Site.create(:nom => @nom , :url => @url ,
+      :user_id => @user_id )
+    # do the next thing
       
 
 
-          #site_id , length=1 , ordre = 1 ; content = "the whole page"
+
+
+    #site_id , length=1 , ordre = 1 ; content = "the whole page"
+      
+
+    @fragment = Fragment.create(:site_id => site[:id] , 
+      :length => 1 , :ordre => 1   , :content => $content )
+
+    redirect_to site_path(site)
             
-    
-          @fragment = Fragment.create(:site_id => site[:id] , 
-           :length => 1 , :ordre => 1   , :content => content )
-    
-          redirect_to site_path(site)
-          end  
-        end
   end
 
   def edit
@@ -63,9 +44,13 @@ class SitesController < ApplicationController
   end
 
   def update
-    @site.update(site_params)
     
+    @site=Site.where(id:params[:id]).update(:nom => @nom , :url => @url ,
+      :user_id => @user_id)
+    Fragment.where(site_id:params[:id]).update(:site_id => params[:id] , 
+      :length => 1 , :ordre => 1   , :content => $content)
     redirect_to site_path(@site)
+    
   end
 
   def destroy
@@ -85,5 +70,55 @@ class SitesController < ApplicationController
   def current_site
     @site = Site.find(params[:id])
   end
+
+  def Before_Create_Update
+    begin
+        $e=""#connected
+        connexion = Nokogiri::HTML(open('https://www.google.com/'))
+        rescue Exception => $e
+      end     
+        
+        if ($e!="")
+        redirect_to request.original_fullpath
+        else
+          begin
+          $e=""#connected
+          content = Nokogiri::HTML(open(site_params[:url]))
+          rescue Exception => $e
+          end     
+          
+          if ($e!="") #not connected
+          redirect_to request.original_fullpath
+          else
+          #:nom , :url , :user_id
+          @user_id = current_user[:id] 
+          @url = site_params[:url] 
+          @nom = site_params[:nom]
+          # do the next thing
+          $content=content.to_s  
+          
+          
+          
+          end  
+        end
+
+
+
+  end  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 end
