d16e49e3e4cb6560fbeb87ec6a797970d0d164f5
Z M Y <modtafrik@gmail.com>
2019-05-11 19:25:58 +0100

    trying to fix heroku upload , stage 6

--- db/migrate/20190503114847_change_time_to_watch_default.rb
+++ db/migrate/20190503114847_change_time_to_watch_default.rb
@@ -1,5 +1,11 @@
 class ChangeTimeToWatchDefault < ActiveRecord::Migration[5.2]
-  def change
-  change_column_default :sites, :time_to_watch,from:0,to:300
+  def up
+  change_column_default :sites, :time_to_watch,from:300,to:300
+  change_column :sites, :time_to_watch, :integer
+  end
+  
+  def down
+  change_column_default :sites, :time_to_watch,from:300,to:0
+  change_column :sites, :time_to_watch, :time
   end
 end
